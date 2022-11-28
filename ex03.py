fb3ca58e9707887e1657d44f9e0520e630a89f55
jose baptista <pv26045@estgl.ipv.pt>
2022-11-28 15:13:36 +0000

    New Exercises

---
+++ ex03.py
@@ -0,0 +1,11 @@
+a = int(input("Digite o primeiro valor:"))
+b = int(input("Digite o segundo valor:"))
+c = int(input("Digite o terceiro valor:"))
+
+maior = a
+if b > a and b > c:
+    maior = b
+if c > a and c > b:
+    maior = c
+
+print(f"O maior número digitado foi {maior}")
